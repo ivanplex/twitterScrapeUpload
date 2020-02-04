fa089c7893e63965b757015c5c2f08ae492c7799
Ivan Chan <ivan@ivanplex.com>
2020-02-04 14:04:36 +0000

    Cloud function

--- database.py
+++ database.py
@@ -3,15 +3,40 @@ from dotenv import load_dotenv
 
 load_dotenv() # Load Service Account Key location
 
-client = bigquery.Client()
+class BigQueryDatabase:
 
-table_id = "cogniflare-rd.g09_social_media.twitter"
+    def __init__(self, project_id, dataset_id, table_id):
+        self.client = bigquery.Client()
+        self.project_id = project_id
+        self.dataset_id = dataset_id
+        self.table_id = table_id
 
-schema = [
-    bigquery.SchemaField("tweet_id", "INTEGER", mode="REQUIRED"),
-    bigquery.SchemaField("body", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("datetime", "DATETIME")
-]
+    def doesTableExist(self, project_id, dataset_id, table_id):
+        # If table exist
+        try:
+            bigquery.tables().get(
+                projectId=project_id, 
+                datasetId=dataset_id,
+                tableId=table_id).execute()
+            return True
+        except HttpError, err
+            if err.resp.status <> 404:
+            raise
+            return False
 
-table = bigquery.Table(table_id, schema=schema)
-table = client.create_table(table)  # Make an API request.
+    def createTable(self):
+        # table_path = "cogniflare-rd.g09_social_media.twitter"
+        table_path = self.project_id + self.dataset_id + self.table_id
+
+        schema = [
+            bigquery.SchemaField("tweet_id", "INTEGER", mode="REQUIRED"),
+            bigquery.SchemaField("body", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("datetime", "DATETIME")
+        ]
+
+        table = bigquery.Table(table_path, schema=schema)
+        table = self.client.create_table(table)  # Make an API request.
+
+    
+
+
