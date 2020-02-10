201b4702dc94498eac469ac757cf8475df4b19b9
Ivan Chan <ivan@ivanplex.com>
2020-02-10 11:21:04 +0000

    New upload function

--- cf.py
+++ cf.py
@@ -6,15 +6,35 @@ import datetime as dt
 import csv
 
 
+def uploadToGCS(bucket_name, source_file_name, destination_blob_name):
+    """
+    Uploads a file to the bucket.
+    
+    Taken from example:
+    https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-code-sample
+    """
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    print("Preparing to upload.")
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
 def scrapeByTheHour(request):
     print("Begin scanning for tweets related to "+ "vodafone"+ ": "+ str(dt.datetime.now()))
-    list_of_tweets = query_tweets("vodafone", 1, begindate=dt.date(2020,2,4), enddate=dt.date(2020,2,5))
+    list_of_tweets = query_tweets("vodafone", 10, begindate=dt.date(2020,2,4), enddate=dt.date(2020,2,5))
     print("Scanning complete: "+ str(dt.datetime.now()))
     df = pd.DataFrame(columns=['id', 'user_id', 'body', 'timestamp'])
-
-    storage_client = storage.Client()
-    bucket_name = "g09-datasets"
-    bucket = storage.Bucket(bucket_name)
 
     for tweet in list_of_tweets:
         lang = detect(tweet.text)
@@ -30,5 +50,7 @@ def scrapeByTheHour(request):
             )
 
     df.to_csv('/tmp/blob.csv')
-    blob = bucket.blob('twitter/crawl/2020_02_05.csv')
-    blob.upload_from_filename('/tmp/blob.csv')
+    uploadToGCS('g09-datasets', '/tmp/blob.csv', 'twitter/crawl/2020_02_04.csv')
+
+
+scrapeByTheHour(None)
