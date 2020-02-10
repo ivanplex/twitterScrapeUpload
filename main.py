318370581abb413dfda22674b30edd61bb539dc5
Ivan Chan <ivan@ivanplex.com>
2020-02-10 15:13:12 +0000

    Remove size evaluation script

--- main.py
+++ main.py
@@ -34,38 +34,39 @@ def scrapeByTheHour(request=None, limit=None):
     print("Begin scanning for tweets related to "+ "vodafone"+ ": "+ str(dt.datetime.now()))
     list_of_tweets = query_tweets("vodafone", limit=limit, begindate=dt.date(2020,2,4), enddate=dt.date(2020,2,5))
     print("Scanning complete: "+ str(dt.datetime.now()))
-    df = pd.DataFrame(columns=['id', 'tweet_url', 'timestamp', 'timestamp_epochs', 'user_id', 'username', 'user_screen_name', 'body', 'body_html' ,'body_links', 'body_hashtags', 'action_likes', 'action_retweets', 'action_replies', 'action_is_replied', 'action_is_reply_to', 'action_parent_tweet_id', 'action_reply_to_users'])
+    df = pd.DataFrame(columns=['id', 'tweet_url', 'timestamp', 'timestamp_epochs', 'user_id', 'username', 'user_screen_name', 'body', 'body_html' ,'body_links', 'body_hashtags', 'body_lang', 'action_likes', 'action_retweets', 'action_replies', 'action_is_replied', 'action_is_reply_to', 'action_parent_tweet_id', 'action_reply_to_users'])
 
     for tweet in list_of_tweets:
         try:
             lang = detect(tweet.text)
-            if lang == 'en':
-                df = df.append(
-                    {
-                    'id':                       tweet.tweet_id,
-                    'tweet_url':                tweet.tweet_url,
-                    'timestamp':                tweet.timestamp,
-                    'timestamp_epochs':         tweet.timestamp_epochs,
+            df = df.append(
+                {
+                'id':                       tweet.tweet_id,
+                'tweet_url':                tweet.tweet_url,
+                'timestamp':                tweet.timestamp,
+                'timestamp_epochs':         tweet.timestamp_epochs,
 
-                    'user_id':                  tweet.user_id,
-                    'username':                 tweet.username,
-                    'user_screen_name':         tweet.screen_name,
+                'user_id':                  tweet.user_id,
+                'username':                 tweet.username,
+                'user_screen_name':         tweet.screen_name,
 
-                    'body':                     tweet.text,
-                    'body_html':                tweet.text_html,
-                    'body_links':               tweet.links,
-                    'body_hashtags':            tweet.hashtags,
+                'body':                     tweet.text,
+                'body_html':                tweet.text_html,
+                'body_links':               tweet.links,
+                'body_hashtags':            tweet.hashtags,
 
-                    'action_likes':             tweet.likes,
-                    'action_retweets':          tweet.retweets,
-                    'action_replies':           tweet.replies,
-                    'action_is_replied':        tweet.is_replied,
-                    'action_is_reply_to':       tweet.is_reply_to,
-                    'action_parent_tweet_id':   tweet.parent_tweet_id,
-                    'action_reply_to_users':    tweet.reply_to_users,
-                    },
-                    ignore_index=True
-                )
+                'body_lang':                lang,
+
+                'action_likes':             tweet.likes,
+                'action_retweets':          tweet.retweets,
+                'action_replies':           tweet.replies,
+                'action_is_replied':        tweet.is_replied,
+                'action_is_reply_to':       tweet.is_reply_to,
+                'action_parent_tweet_id':   tweet.parent_tweet_id,
+                'action_reply_to_users':    tweet.reply_to_users,
+                },
+                ignore_index=True
+            )
         except:
             print("Error detecting lang: "+tweet.text)
 
